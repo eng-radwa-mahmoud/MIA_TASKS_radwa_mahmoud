170fb69329a8cb214147cf16d15d6a63ca90b51a
radwa mahmoud <89464866+eng-radwa-mahmoud@users.noreply.github.com>
2021-11-09 22:48:13 +0200

    Update task_3_subscriber.ino

--- Third_week_Task/task_3_subscriber/task_3_subscriber.ino
+++ Third_week_Task/task_3_subscriber/task_3_subscriber.ino
@@ -2,9 +2,11 @@
 #include <std_msgs/Int32.h>
 ros::NodeHandle v_subscriber;
 long long int set_point;
-void messageCb(const std_msgs::int::ConstPtr& msg);
+void messageCb(const std_msgs::Int32::ConstPtr& msg);
 long long counter1=0;
-ros::Subscriber<std_msgs::Int32> sub("set_point", &messageCb ); 
+ros::Subscriber<std_msgs::Int32> sub("set_point", messageCb ); 
+long int current_speed;
+long int current_error=0,sum_error=0,previous_error=0,delta_error=0,pwm=0;
 void ISRA1() 
 {
   if (digitalRead(PB_0)==digitalRead(PA_7))//check if encoder is moving clockwise or counter clockwise
@@ -32,11 +34,27 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
+  if ((millis()-prev_time)>200)
+  { prev_time=millis();
+    current_speed=(((counter1*60))/.2)/512;
+    counter1=0;
+    current_error=set_point-current_speed;
+    sum_errror=sum_error+current_error;
+    delta_error=current_error-previous_error;
+    previous_error=current_error;
+    pwm=.2*current_error+.5*sum_error+.8*delta_error;
+    digitalWrite(PA_1,HIGH);
+    digitalWrite(PA_1,LOW);
+    if (abs(pwm)>255)
+     analogWrite(PA_0,255);
+     else
+    analogWrite(PA_0,abs(pwm));
+  }
+  
   v_subscriber.spinOnce();
-
+  
 }
-void messageCb(const std_msgs::int::ConstPtr& msg)
+void messageCb(const std_msgs::Int32::ConstPtr& msg)
 {
  set_point= msg->data.c_int();
 }
